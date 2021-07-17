35ff7b1ed283ffd611992b5e869d25608595a1cb
Ajay <Ajay@192.168.2.101>
2021-07-17 12:14:38 +0530

    first commit

---
+++ ServiceThree/src/main/java/com/a2y/spring/service/three/ServiceThreeApplication.java
@@ -0,0 +1,13 @@
+package com.a2y.spring.service.three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceThreeApplication.class, args);
+	}
+
+}
---
+++ ServiceThree/src/test/java/com/a2y/spring/service/three/ServiceThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2y.spring.service.three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
